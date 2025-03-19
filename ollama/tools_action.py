0af53517af00af411acdf3a0f53980c11cbdf578
huangye <huangye@cvte.com>
2025-03-19 15:12:36 +0800

    增加简约action demo，log，修复没有把assistant回复内容再次传递的问题

--- ollama/tools_action.py
+++ ollama/tools_action.py
@@ -1,18 +1,15 @@
 from ollama import ChatResponse, chat
 
-# llm = "llama3.2"
-llm = 'qwq'
-
+llm = "llama3.2"
+# llm = 'qwq'
 
 def air(value: int) -> int:
-    print("空调调到 %d 度" % value)
+    print("----- air函数。value类型： %s, 自己执行空调调到 %s 度." % (type(value), value))
     return 0
-
 
 def fan(value: int) -> int:
-    print("风扇调到 %d 档" % value)
+    print("----- air函数。value类型： %s, 自己执行风扇调到 %s 档" % (type(value), value))
     return 0
-
 
 air_tool = {
     "type": "function",
@@ -44,33 +41,28 @@ fan_tool = {
     },
 }
 
-
 messages = [{"role": "user", "content": "帮我把风扇调到5档，空调调到29度."}]
-
 print("输入:", messages[0]["content"])
-
 available_functions = {
     "air": air,
     "fan": fan,
 }
 
 response: ChatResponse = chat(llm, messages=messages, tools=[air_tool, fan_tool])
-
 print("得到response: 对象", response)
-
 f_name = ""
 
 if response.message.tool_calls:
-    # There may be multiple tool calls in the response
+    # llama append
+    messages.append(response.message)
+
     for tool in response.message.tool_calls:
-        # Ensure the function is available, and then call it
         if function_to_call := available_functions.get(tool.function.name):
             print("Calling function:", tool.function.name)
             print("Arguments:", tool.function.arguments)
             output = function_to_call(**tool.function.arguments)
             print("Function output:", output)
             f_name = tool.function.name
-            messages.append(response.message)
             if (output == 0):
                 msg = '调节成功'
             else:
@@ -79,16 +71,9 @@ if response.message.tool_calls:
         else:
             print("Function", tool.function.name, "not found")
 
-# Only needed to chat with the modelusing the tool call results
 if response.message.tool_calls:
-    # # Add the function response to messages for the model to use
-    # messages.append(response.message)
-    # # 把参数给llm
-    # messages.append({"role": "tool", "content": str(output), "name": f_name})
-
-    # Get final response from model with function outputs
+    print("再次问ai。messages", messages)
     final_response = chat(llm, messages=messages)
-    print("Final response:", final_response.message.content)
-
+    print("Final response:", final_response)
 else:
     print("No tool calls returned from model")
